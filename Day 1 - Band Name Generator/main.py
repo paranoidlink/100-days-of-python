b4f5cd2421652279454bc0d5a805d383e35d665f
paranoidlink <ben.atherton.1998@gmail.com>
2025-03-12 14:47:16 +0000

    Day 1

---
+++ Day 1 - Band Name Generator/main.py
@@ -0,0 +1,4 @@
+print("Hello and Welcome to the Band Name Generator")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's the name of your pet?\n")
+print("Your band name is: " + city_name + " " + pet_name + "!")
